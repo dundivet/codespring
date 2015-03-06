2452fed5d8940ac1cb92d2c5d2f418cabbb8e5b4
Dundivet <dundivet@gmail.com>
2015-03-05 19:17:11 -0500

    Adicionando librerías para frontend y javascript

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new Caribbean\TourismBundle\TourismBundle(),
             new Caribbean\SecurityBundle\CaribbeanSecurityBundle(),
             new FOS\UserBundle\FOSUserBundle(),
+            new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
